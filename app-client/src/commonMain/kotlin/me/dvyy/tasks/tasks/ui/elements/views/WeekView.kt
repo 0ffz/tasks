96cd176c0462a4e1363d453db949de0015d12605
Danielle Voznyy <dan.voznyy@gmail.com>
2026-05-12 21:36:10 -0400

    fix: Temporary fix for scrolling to today on android

--- app-client/src/commonMain/kotlin/me/dvyy/tasks/tasks/ui/elements/views/WeekView.kt
+++ app-client/src/commonMain/kotlin/me/dvyy/tasks/tasks/ui/elements/views/WeekView.kt
@@ -4,12 +4,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraintsScope
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.relocation.BringIntoViewRequester
+import androidx.compose.foundation.relocation.bringIntoViewRequester
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import kotlinx.coroutines.delay
 import kotlinx.datetime.DatePeriod
 import kotlinx.datetime.plus
 import me.dvyy.tasks.app.ui.AppState
@@ -20,6 +25,7 @@ import me.dvyy.tasks.tasks.ui.elements.list.rememberProjectDisplayOptions
 import me.dvyy.tasks.time.TimeViewModel
 import org.koin.compose.koinInject
 import org.koin.compose.viewmodel.koinViewModel
+import kotlin.time.Duration.Companion.seconds
 
 @Composable
 fun BoxWithConstraintsScope.WeekView(
@@ -34,6 +40,15 @@ fun BoxWithConstraintsScope.WeekView(
 
     val scrollState = rememberScrollState()
     val today by time.today.collectAsState()
+
+    //    LaunchedEffect(scrollToPosition) {
+//        delay(1.seconds)
+//        if(!scrolled) {
+//            scrolled = true
+//            scrollState.scrollTo(scrollToPosition.toInt())
+//            Logger.i { "Scrolling to $scrollToPosition" }
+//        }
+//    }
     Box(Modifier.height(maxHeight)) {
         NonlazyGrid(
             columns = columns,
@@ -43,7 +58,13 @@ fun BoxWithConstraintsScope.WeekView(
             val day = weekStart.plus(DatePeriod(days = dayIndex))
             val isToday = day == today
             val listId = ListId.forDate(day)
+            val bringIntoViewRequester = remember { BringIntoViewRequester() }
 
+            //TODO rework to just use a single lazycolumn and scroll to the element for today at startup
+            LaunchedEffect(Unit) {
+                delay(0.5.seconds)
+                if (isToday) bringIntoViewRequester.bringIntoView()
+            }
             Project(
                 listId,
                 displayOptions = rememberProjectDisplayOptions(
@@ -51,7 +72,9 @@ fun BoxWithConstraintsScope.WeekView(
                     scrollable = false,
                     fullHeight = !isSmall,
                 ),
-//                modifier = Modifier.onGloballyPositioned { coords -> scrollToPosition = coords.positionInRoot().y }
+                modifier = Modifier.bringIntoViewRequester(bringIntoViewRequester)/* { coords ->
+                    scrollToPosition = coords.positionInRoot().y
+                }*/
             )
 
 //            LaunchedEffect(Unit) {
